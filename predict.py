8737644b0fb6ebee69ec205253307e25532fb4d4
UnicornKevin <2406987078@qq.com>
2022-05-25 18:57:31 +0800

    解决了车牌第一个字识别错的情况

--- predict.py
+++ predict.py
@@ -461,7 +461,6 @@ class CardPredictor:
             img = self.__gamma_transform(img, 1)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             img = cv2.equalizeHist(img)  # 直方图均衡
-            img = cv2.GaussianBlur(img, (blur, blur), 0)
             self.__imgshow_and_key_detect("equality", img)  # 显示
         self.__homofilter(img)
         self.__imgshow_and_key_detect("grey_after_homo", img)  # 显示
@@ -478,7 +477,7 @@ class CardPredictor:
         # 找到图像边缘
         ret, img_thresh = cv2.threshold(img_opening, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         img_edge = cv2.Canny(img_thresh, 100, 200)
-        self.__imgshow_and_key_detect("canny", img_thresh)
+        self.__imgshow_and_key_detect("canny", img_edge)
         # 使用开运算和闭运算让图像边缘成为一个整体
         kernel = np.ones((self.cfg["morphologyr"], self.cfg["morphologyc"]), np.uint8)
         img_edge1 = cv2.morphologyEx(img_edge, cv2.MORPH_CLOSE, kernel)
@@ -519,7 +518,7 @@ class CardPredictor:
                 angle = 1
             else:
                 angle = rect[2]
-            rect = (rect[0], (rect[1][0] + 5, rect[1][1] + 5), angle)  # 扩大范围，避免车牌边缘被排除
+            rect = (rect[0], (rect[1][0] + 12, rect[1][1] + 8), angle)  # 扩大范围，避免车牌边缘被排除
 
             box = cv2.boxPoints(rect)
             heigth_point = right_point = [0, 0]
@@ -624,6 +623,7 @@ class CardPredictor:
                 xl = 0
                 xr = col_num
                 need_accurate = True
+
             card_imgs[card_index] = card_img[yl:yh, xl:xr] if color != "green" or yl < (yh - yl) // 4 else card_img[
                                                                                                            yl - (
                                                                                                                    yh - yl) // 4:yh,
@@ -673,7 +673,7 @@ class CardPredictor:
                 # 查找垂直直方图波峰
                 row_num, col_num = gray_img.shape[:2]
                 # 去掉车牌上下边缘1个像素，避免白边影响阈值判断
-                gray_img = gray_img[1:row_num - 1]
+                gray_img = gray_img[1:row_num]
                 y_histogram = np.sum(gray_img, axis=0)
                 y_min = np.min(y_histogram)
                 y_average = np.sum(y_histogram) / y_histogram.shape[0]
@@ -736,6 +736,7 @@ class CardPredictor:
                     else:
                         resp = self.model.predict(part_card)
                         charactor = chr(int(resp[0]))
+                    self.__imgshow_and_key_detect("part", part_card_old)
                     # 判断最后一个数是否是车牌边缘，假设车牌边缘被认为是1
                     if charactor == "1" and i == len(part_cards) - 1:
                         if part_card_old.shape[0] / part_card_old.shape[1] >= 8:  # 1太细，认为是边缘
